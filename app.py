628e4f785569fbe6cd188221c5aabe2de5dab453
Andy <sterfulandy@gmail.com>
2025-04-08 11:11:21 -0600

    Moving the models to their own folder

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ import pickle
 import numpy as np
 import pandas as pd
 
-with open('modelo_svm.pkl', 'rb') as model_file:
+with open('models/modelo_svm.pkl', 'rb') as model_file:
     model = pickle.load(model_file)
 
-with open('scaler.pkl', 'rb') as scaler_file:
+with open('models/scaler.pkl', 'rb') as scaler_file:
     scaler = pickle.load(scaler_file)
 
 feature_info = {
